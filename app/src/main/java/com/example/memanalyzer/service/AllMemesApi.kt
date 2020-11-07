2a7646302e3c5d68079c980655909a9815fd24f8
arranay <valeriakhlyzoba@gmail.com>
2020-11-07 21:02:00 +0300

    added POST request

--- app/src/main/java/com/example/memanalyzer/service/AllMemesApi.kt
+++ app/src/main/java/com/example/memanalyzer/service/AllMemesApi.kt
@@ -1,11 +1,19 @@
 package com.example.memanalyzer.service
 
+import com.example.memanalyzer.model.Answers
 import com.example.memanalyzer.model.Img
+import com.example.memanalyzer.model.Result
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface AllMemesApi {
 
-    @GET("AllMemes")
+    @GET("Memes/AllMemes")
     fun getAllMemes(): Call<List<Img>>
+
+    @POST("Memes/Result")
+    fun getResult(@Body answers: List<Answers>): Call<Result>
+
 }
